4b47439916ba0adecae4f25abb2fcafa9edfc9da
Zygrade <hsenday.6991@gmail.com>
2018-12-27 20:05:24 +0530

    minor changes

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const User = mongoose.model('user');
+const User = mongoose.model('User');
 
 exports.authenticate = (email,password) => {
     return new Promise(async (resolve,reject) => {
